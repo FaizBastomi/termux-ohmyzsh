3581cd255983ca7f14983cb0504ac35a67d17b5b
ファイズ <64179402+FaizBastomi@users.noreply.github.com>
2021-11-30 19:40:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,24 @@ termux-setup-storage
 
 RED='\033[0;31m'
 GRN='\033[0;32m'
+CY='\033[0;36m'
 NC='\033[0m'
 
 apt update
 apt install -y git zsh neofetch
-git clone https://github.com/Cabbagec/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1
 
+echo -e "${GRN}Cloning ${CY}termux-ohmyzsh${NC}"
+git clone https://github.com/Cabbagec/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1 --quiet
+
+echo -e "${GRN}Backup ${CY}.termux${GRN} folder${NC}"
 mv "$HOME/.termux" "$HOME/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+echo -e "${GRN}Copy ${CY}termux${GRN} folder from termux-ohmyzsh${NC}"
 cp -R "$HOME/termux-ohmyzsh/.termux" "$HOME/.termux"
 echo -e "${RED}Deleting motd...${NC}"
 rm $HOME/../usr/etc/motd
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
+echo -e "${GRN}Cloning ${CY}oh-my-zsh${GRN} repository${NC}"
+git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1 --quiet
 mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
 cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
 sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
@@ -29,12 +35,13 @@ echo "alias pkgcc='pkg clean'" >> "$HOME/.zshrc"
 echo "alias pkgup='pkg update -y'" >> "$HOME/.zshrc"
 echo "neofetch --ascii_distro 'Gentoo'" >> "$HOME/.zshrc"
 
+echo -e "${GRN}Cloning ${CY}zsh-syntax-highlighting${NC}"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
 echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
 
 chsh -s zsh
 
-echo "oh-my-zsh install complete!\nChoose your color scheme now~"
+echo -e "${GRN}oh-my-zsh${NC} install complete!\nChoose your color scheme now~"
 $HOME/.termux/colors.sh
 
 echo "Choose your font now~"
